07d13cc7385efe8c0195ca8006bd8692cb6f18dd
JMantello <38162257+JMantello@users.noreply.github.com>
2022-01-08 13:52:43 -0800

    Copy files from web page folder to here

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+function checkPreferredTheme() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.getElementById("darkThemeCheckbox").checked = true;
+    }
+    setCheckedTheme();
+}
+
+function setCheckedTheme() {
+    if(document.getElementById("darkThemeCheckbox").checked) {
+        addDarkTheme();
+    } else {
+        removeDarkTheme();
+    }
+}
+
+function addDarkTheme() {
+    document.body.classList.add("dark-theme");
+}
+
+function removeDarkTheme() {
+    document.body.classList.remove("dark-theme");
+}
