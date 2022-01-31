352666637ff8b1d85f033c5d96e57bd5c92d61e3
JMantello <38162257+JMantello@users.noreply.github.com>
2022-01-30 17:29:43 -0800

    Refactoring and styling updates

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
-
-
-function checkPreferredTheme() {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.getElementById("darkThemeCheckbox").checked = true;
-    }
-    setCheckedTheme();
+function getPreferredTheme() {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    document.getElementById("darkThemeCheckbox").checked = true;
+  }
+  setColorTheme();
 }
 
-function setCheckedTheme() {
-    if(document.getElementById("darkThemeCheckbox").checked) {
-        addDarkTheme();
-    } else {
-        removeDarkTheme();
-    }
+function setColorTheme() {
+  if (document.getElementById("darkThemeCheckbox").checked) {
+    addDarkTheme();
+  } else {
+    removeDarkTheme();
+  }
 }
 
 function addDarkTheme() {
-    document.body.classList.add("dark-theme");
+  document.body.classList.add("dark-theme");
 }
 
 function removeDarkTheme() {
-    document.body.classList.remove("dark-theme");
+  document.body.classList.remove("dark-theme");
 }
